94e229f4e21f532f5e6ff2cfd9a3ef1af3261d96
wrakelft <gleb@alfn.ru>
2026-04-09 19:47:46 +0300

    initial commit

---
+++ blps1/src/main/java/ru/itmo/blps1/Blps1Application.java
@@ -0,0 +1,13 @@
+package ru.itmo.blps1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blps1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blps1Application.class, args);
+    }
+
+}
---
+++ blps1/src/test/java/ru/itmo/blps1/Blps1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.blps1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blps1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
